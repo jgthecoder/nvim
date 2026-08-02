64ff2a66319d3956f0bf0a13dd5c8e50726f55d5
Joshua Guzman <jgthecoder@gmail.com>
2026-08-02 16:02:00 -0400

    remove custom gitsigns

--- lua/josh/plugins/git.lua
+++ lua/josh/plugins/git.lua
@@ -28,13 +28,13 @@ return {
     "lewis6991/gitsigns.nvim",
     event = "BufRead",
     opts = {
-      signs = {
-        add = { text = "+" },
-        change = { text = "~" },
-        delete = { text = "_" },
-        topdelete = { text = "‾" },
-        changedelete = { text = "~" },
-      },
+      -- signs = {
+      --   add = { text = "+" },
+      --   change = { text = "~" },
+      --   delete = { text = "_" },
+      --   topdelete = { text = "‾" },
+      --   changedelete = { text = "~" },
+      -- },
     },
   },
 }
